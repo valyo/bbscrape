5b8a1c79df80df62115d57f4a9a07a9c915a5207
Valentin Georgiev <valentin.georgiev@farmbio.uu.se>
2017-12-09 17:57:46 +0100

    added check_ads.py to the bash script

--- get_cars_links.sh
+++ get_cars_links.sh
@@ -3,7 +3,18 @@
 PYTHON=$(which python)
 WDIR="$HOME/bytbil.com"
 
+
+# scrape bytbil.com page-by-page and add new ads links to DB
+echo "get_cars log" > $WDIR/cron.log
 $PYTHON $WDIR/get_cars.py > $WDIR/cron.log 2> $WDIR/cron.err
+
+
+# check for ads that have gone away and set end_date
+# for them in DB
+echo "check_ads log" > $WDIR/cron.log
+$PYTHON $WDIR/check_ads.py > $WDIR/cron.log 2> $WDIR/cron.err
+
+
 # TODO
 # if the above command succeeded
 # make a copy of the DB file with today's date in the filename 
