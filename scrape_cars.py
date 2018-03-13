91df80fd88869d082ce363cb8f58c04d15db5d2a
Valentin Georgiev <valent@Valentins-MBP.lan>
2018-03-13 11:36:01 +0100

    moved the for loop going through extras inside
    The try-except block - that way the script does not crash if an ad has no extras section

--- scrape_cars.py
+++ scrape_cars.py
@@ -46,7 +46,7 @@ class getCarsData:
 
    def getAdsLinksList(self,cur):
       
-      query = "SELECT link_id,link FROM car_links WHERE annons_id = "n";"
+      query = "SELECT link_id,link FROM car_links WHERE annons_id = 'n';"
       cur.execute(query)
       link = list(cur.fetchall())
       return link
@@ -160,174 +160,174 @@ class getCarsData:
       try:
          # extras = soup.find("div", class_="uk-grid uk-grid-width-medium-1-3").find_all('li')
          extras = soup.find("div", class_="uk-width-1-1 vehicle-detail-equipment-detail").find_all('li')
-      except Exception as e:
-         # e
+      except Exception as e1:
+         print(e1)
          try:
             extras = soup.find("ul", class_="uk-list-space equipment-list").find('li')
             # print(extras.string.split())
             extras = extras.split()
+
+            for e in extras:
+               e = e.string
+               # print(e.string)
+               match = re.search('abs', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['abs'] = "1"
+
+               match = re.search('Klimatanl|AC', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['klima'] = "1"
+
+               match = re.search('Dragkrok', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['dragkrok'] = "1"
+
+               match = re.search('Elhissar', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['elhissar'] = "1"
+
+               match = re.search('Elspeglar', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['elspeglar'] = "1"
+
+               match = re.search('Farth.llare', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['farthallare'] = "1"
+
+               match = re.search('c-l.s', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['c-las'] = "1"
+
+               match = re.search('F.rddator', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['fdator'] = "1"
+
+               match = re.search('LM-f.lgar|aluminium.*', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['alufalg'] = "1"
+
+               match = re.search('Multifunktionsratt', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['multifunktionsratt'] = "1"
+
+               match = re.search('Servostyrning|servo.*', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['servo'] = "1"
+
+               match = re.search('Stolv.rme.*fram|Eluppv.rmda stolar fram', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['stolv-fram'] = "1"
+
+               match = re.search('Svensks.ld', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['svensks'] = "1"
+
+               match = re.search('Yttertemperaturm.tare', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['ytempm'] = "1"
+
+               match = re.search('Muggh.llare', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['muggh'] = "1"
+
+               match = re.search('Spoilerljus|dimljus', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['dimljus'] = "1"
+
+               match = re.search('rattv.rme|Eluppv.rmd ratt', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['rattv'] = "1"
+
+               match = re.search('Led.str.*lkastare', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['ledheadl'] = "1"
+
+               match = re.search('l.der|skin', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['skin'] = "1"
+
+               match = re.search('glas*taklucka', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['lucka'] = "1"
+
+               match = re.search('Parkering.*bak|Backkamera|Parkeringspaket|Backvarnare|Parkeringssensor.*', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['parkassist'] = "1"
+
+               match = re.search('Start.*Stop.*funktion', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['startstop'] = "1"
+
+               match = re.search('.*stol.*minne.*', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['stolminne'] = "1"
+
+               match = re.search('Bluetooth', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['bluetooth'] = "1"
+
+               match = re.search('Larm', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['larm'] = "1"
+
+               match = re.search('Motorv.rmare', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['motorv'] = "1"
+
+               match = re.search('Rails', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['rails'] = "1"
+
+               match = re.search('Vinterhjul.*friktion', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['vinterd-fr'] = "1"
+
+               match = re.search('Vinterhjul.*dub.*|Vinterhjul', e.string, flags=re.IGNORECASE)
+               if match:
+                  if not info['vinterd-fr'] == "1":
+                     info['vinterd-d'] = "1"
+
+               match = re.search('Antisladd', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['antisladd'] = "1"
+
+               match = re.search('Antispinn', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['antispinn'] = "1"
+
+               match = re.search('Regnsensor', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['regnsensor'] = "1"
+
+               match = re.search('Xenon.*', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['xenon'] = "1"
+
+               match = re.search('Airbag', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['airbag'] = "1"
+
+               match = re.search('Navigator|GPS|navi', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['gps'] = "1"
+
+               match = re.search('keyless', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['keyless'] = "1"
+
+               match = re.search('Laddhybrid', e.string, flags=re.IGNORECASE)
+               if match:
+                  info['laddhybrid'] = "1"
+
+               if info['year'] >= 2000:
+                  info['abs'] = "1"
+                  info['airbag'] = "1"
+                  info['fdator'] = "1"
+                  info['elspeglar'] = "1"
+
          except Exception as e1:
-           e
-           # print(e1) 
-      # extras = soup.find("div", class_="uk-grid uk-grid-width-medium-1-3").find_all('li')
-      # print len(extras)
-      for e in extras:
-         e = e.string
-         # print(e.string)
-         match = re.search('abs', e.string, flags=re.IGNORECASE)
-         if match:
-            info['abs'] = "1"
+           print(e1)
 
-         match = re.search('Klimatanl|AC', e.string, flags=re.IGNORECASE)
-         if match:
-            info['klima'] = "1"
-
-         match = re.search('Dragkrok', e.string, flags=re.IGNORECASE)
-         if match:
-            info['dragkrok'] = "1"
-
-         match = re.search('Elhissar', e.string, flags=re.IGNORECASE)
-         if match:
-            info['elhissar'] = "1"
-
-         match = re.search('Elspeglar', e.string, flags=re.IGNORECASE)
-         if match:
-            info['elspeglar'] = "1"
-
-         match = re.search('Farth.llare', e.string, flags=re.IGNORECASE)
-         if match:
-            info['farthallare'] = "1"
-
-         match = re.search('c-l.s', e.string, flags=re.IGNORECASE)
-         if match:
-            info['c-las'] = "1"
-
-         match = re.search('F.rddator', e.string, flags=re.IGNORECASE)
-         if match:
-            info['fdator'] = "1"
-
-         match = re.search('LM-f.lgar|aluminium.*', e.string, flags=re.IGNORECASE)
-         if match:
-            info['alufalg'] = "1"
-
-         match = re.search('Multifunktionsratt', e.string, flags=re.IGNORECASE)
-         if match:
-            info['multifunktionsratt'] = "1"
-
-         match = re.search('Servostyrning|servo.*', e.string, flags=re.IGNORECASE)
-         if match:
-            info['servo'] = "1"
-
-         match = re.search('Stolv.rme.*fram|Eluppv.rmda stolar fram', e.string, flags=re.IGNORECASE)
-         if match:
-            info['stolv-fram'] = "1"
-
-         match = re.search('Svensks.ld', e.string, flags=re.IGNORECASE)
-         if match:
-            info['svensks'] = "1"
-
-         match = re.search('Yttertemperaturm.tare', e.string, flags=re.IGNORECASE)
-         if match:
-            info['ytempm'] = "1"
-
-         match = re.search('Muggh.llare', e.string, flags=re.IGNORECASE)
-         if match:
-            info['muggh'] = "1"
-
-         match = re.search('Spoilerljus|dimljus', e.string, flags=re.IGNORECASE)
-         if match:
-            info['dimljus'] = "1"
-
-         match = re.search('rattv.rme|Eluppv.rmd ratt', e.string, flags=re.IGNORECASE)
-         if match:
-            info['rattv'] = "1"
-
-         match = re.search('Led.str.*lkastare', e.string, flags=re.IGNORECASE)
-         if match:
-            info['ledheadl'] = "1"
-
-         match = re.search('l.der|skin', e.string, flags=re.IGNORECASE)
-         if match:
-            info['skin'] = "1"
-
-         match = re.search('glas*taklucka', e.string, flags=re.IGNORECASE)
-         if match:
-            info['lucka'] = "1"
-
-         match = re.search('Parkering.*bak|Backkamera|Parkeringspaket|Backvarnare|Parkeringssensor.*', e.string, flags=re.IGNORECASE)
-         if match:
-            info['parkassist'] = "1"
-
-         match = re.search('Start.*Stop.*funktion', e.string, flags=re.IGNORECASE)
-         if match:
-            info['startstop'] = "1"
-
-         match = re.search('.*stol.*minne.*', e.string, flags=re.IGNORECASE)
-         if match:
-            info['stolminne'] = "1"
-
-         match = re.search('Bluetooth', e.string, flags=re.IGNORECASE)
-         if match:
-            info['bluetooth'] = "1"
-
-         match = re.search('Larm', e.string, flags=re.IGNORECASE)
-         if match:
-            info['larm'] = "1"
-
-         match = re.search('Motorv.rmare', e.string, flags=re.IGNORECASE)
-         if match:
-            info['motorv'] = "1"
-
-         match = re.search('Rails', e.string, flags=re.IGNORECASE)
-         if match:
-            info['rails'] = "1"
-
-         match = re.search('Vinterhjul.*friktion', e.string, flags=re.IGNORECASE)
-         if match:
-            info['vinterd-fr'] = "1"
-
-         match = re.search('Vinterhjul.*dub.*|Vinterhjul', e.string, flags=re.IGNORECASE)
-         if match:
-            if not info['vinterd-fr'] == "1":
-               info['vinterd-d'] = "1"
-
-         match = re.search('Antisladd', e.string, flags=re.IGNORECASE)
-         if match:
-            info['antisladd'] = "1"
-
-         match = re.search('Antispinn', e.string, flags=re.IGNORECASE)
-         if match:
-            info['antispinn'] = "1"
-
-         match = re.search('Regnsensor', e.string, flags=re.IGNORECASE)
-         if match:
-            info['regnsensor'] = "1"
-
-         match = re.search('Xenon.*', e.string, flags=re.IGNORECASE)
-         if match:
-            info['xenon'] = "1"
-
-         match = re.search('Airbag', e.string, flags=re.IGNORECASE)
-         if match:
-            info['airbag'] = "1"
-
-         match = re.search('Navigator|GPS|navi', e.string, flags=re.IGNORECASE)
-         if match:
-            info['gps'] = "1"
-
-         match = re.search('keyless', e.string, flags=re.IGNORECASE)
-         if match:
-            info['keyless'] = "1"
-
-         match = re.search('Laddhybrid', e.string, flags=re.IGNORECASE)
-         if match:
-            info['laddhybrid'] = "1"
-
-         if info['year'] >= 2000:
-            info['abs'] = "1"
-            info['airbag'] = "1"
-            info['fdator'] = "1"
-            info['elspeglar'] = "1"
 
    def get_volume_hp(self,soup):
       key_list = []
@@ -580,6 +580,7 @@ if __name__ == "__main__":
 
    # connect to db and create a  cursor
    db = getCars.connectDB()
+   db.text_factory = str
    cur = db.cursor()
 
    # set current date
@@ -612,7 +613,7 @@ if __name__ == "__main__":
       getCarsData.initInfo()
 
       # ad[0] is link_id and ad[1] is the link string
-      # print(ad[1])
+      print(ad[1])
 
       #get the annouce page
       soup = getCarsData.getAdPage(ad[1])
@@ -632,6 +633,13 @@ if __name__ == "__main__":
          getCarsData.get_details(soup)
          getCarsData.get_volume_hp(soup)
          # getCarsData.print_data(info)
+         if info['color'] == None:
+            info['color'] = "NaN"
+         # for k,v in info.iteritems():
+         #    v = v.decode("utf-8")
+         #    print(type(v))
+         #    print(v)
+
          query = "INSERT INTO cars_data (annons_id,awd,abs,airbag,alufalg,antisladd,antispinn,auto,bluetooth,c_las,co2,color,dimljus,dragkrok,eco,elhissar,elspeglar,farthallare,fdator,fuel_b_hybrid,fuel_bensin,fuel_d_hybrid,fuel_diesel,gps,itrafik,keyless,klima,laddhybrid,larm,ledheadl,lucka,make,mileage,model,motor,motorv,muggh,multifunktionsratt,parkassist,power,pris,rails,rattv,regnr,regnsensor,servo,skin,spec,startstop,stolminne,stolv_fram,svensks,vikt,vinterd_d,vinterd_fr,xenon,year,ytempm) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);"
 
          try:
@@ -639,7 +647,7 @@ if __name__ == "__main__":
             cur.execute(query, ([ad[0]] + ['{1}'.format(k, v) for k,v in sorted(info.iteritems())]))
             db.commit()
          except Exception as e:
-
+            print("INSERT query")
             print(e)
             # print link
             # stop = True
